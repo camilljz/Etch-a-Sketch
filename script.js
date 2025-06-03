068a854390125c74c92fb1edcdc7a02a1dd5438f
camilljz <106601649+camilljz@users.noreply.github.com>
2025-06-03 15:22:38 +0200

    Added opacity change, decrease by 10 percent

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-document.body.style.backgroundColor = 'lightblue'
 const buttonWrap = document.createElement('div')
 document.body.appendChild(buttonWrap)
 const container = document.createElement('div')
 document.body.appendChild(container)
 
-
+document.body.style.margin = '0px'
+document.body.style.padding = '0px'
 const button = document.createElement('button')
 buttonWrap.append(button)
 buttonWrap.style.display = 'flex'
@@ -15,6 +15,8 @@ button.textContent = 'Change Grid'
 button.style.fontFamily = 'Courier New'
 button.style.fontWeight = '1000'
 button.style.fontSize = '25px'
+
+
 
 
 for (i = 0; i<16*16; i++) {
@@ -28,21 +30,27 @@ const boxes = document.querySelectorAll('.boxes')
 
 let width = (100/16)
 boxes.forEach (box => {
+    box.style.backgroundColor = 'lightblue'
     box.style.width = width+'%'
     box.style['aspectRatio'] ='1/1'
     box.style['display'] = 'flex'
     box.style.boxSizing = 'border-box'
     box.style.margin = '0px'
     box.style.padding = '0px'
+    box.style.opacity = '1'
     
 })
 
 container.style['display'] = 'flex'
 container.style['flex-wrap'] = 'wrap'
-container.style['backgroundColor'] = 'lightblue'
 container.style.margin = '0px'
 container.style.padding = '0px'
 
+function change(box) {
+    let opacity = parseFloat(box.style.opacity) || 1
+    let change = Math.max(0, opacity-0.1)
+    box.style.opacity = change.toString()
+}
 
 const allBoxes = document.querySelectorAll('.boxes')
 allBoxes.forEach((box) => {
@@ -51,9 +59,7 @@ allBoxes.forEach((box) => {
     })
 })
 
-function change(box) {
-    box.style['backgroundColor'] = 'white'
-}
+
 
 button.addEventListener("click", (e) => {
     let boxwish = prompt('How many boxes do you want on a box x box basis?', 'Enter number')
@@ -69,11 +75,13 @@ button.addEventListener("click", (e) => {
         const boxes = document.querySelectorAll('.boxes');
 
         boxes.forEach (box => {
+            box.style.backgroundColor = 'lightblue'
             box.style.width = width+'%'
             box.style.aspectRatio = '1/1'
             box.style.boxSizing = 'border-box'
             box.style.margin = '0px'
             box.style.padding = '0px'
+            box.style.opacity = '1'
 
         })
 
